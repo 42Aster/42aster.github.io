e2f5e119cb4e7b4b2a9e0f57a3639f84fde7f37c
42Aster <brainbaked7@gmail.com>
2025-07-20 22:22:44 +0200

    frame + minor design changes

--- script.js
+++ script.js
@@ -259,38 +259,7 @@ function displayItems(items) {
       <h2>${item.name}<br>*Masterwork*</h2>
       <div class="corrupt-container">
         <img src="${item.image}" alt="${item.name}" class="corrupt-image">
-
-        <svg class="corrupt-overlay" viewBox="0 0 64 64" width="64" height="64" preserveAspectRatio="none">
-          <defs>
-            <filter id="veinCorruption" x="0" y="0" width="100%" height="100%">
-              <!-- High-detail fractal noise -->
-              <feTurbulence type="fractalNoise" baseFrequency="0.45" numOctaves="4" result="turbulence" />
-
-              <!-- Strong contrast boost -->
-              <feComponentTransfer in="turbulence" result="highContrast">
-                <feFuncR type="gamma" amplitude="3" exponent="0.9" offset="0"/>
-                <feFuncG type="gamma" amplitude="2" exponent="1" offset="0"/>
-                <feFuncB type="gamma" amplitude="1" exponent="1.3" offset="0"/>
-                <feFuncA type="linear" slope="1"/>
-              </feComponentTransfer>
-
-              <!-- Vein coloring: intense reds & oranges -->
-              <feColorMatrix in="highContrast" type="matrix"
-                values="2.5 0.4 0   0  0
-                        0.3 1.8 0.1 0  0
-                        0.1 0.2 0.5 0  0
-                        0   0   0   1  0"
-                result="coloredVeins" />
-
-              <!-- Blend with source image -->
-              <feBlend in="SourceGraphic" in2="coloredVeins" mode="screen"/>
-            </filter>
-          </defs>
-
-          <!-- Rectangle that applies the filter -->
-          <rect x="0" y="0" width="64" height="64" fill="transparent" filter="url(#veinCorruption)" />
-        </svg>
-
+        <img src="styles/frame.png" alt="Frame" class="frame-overlay">
       </div>
 
       ${item.plaintext
